9a56dbc2dd1b1f2c2051b782a416c72fec401992
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2025-02-04 01:36:48 +0100

    add function to retrieve available models from Ollama client

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.prompt_values import PromptValue
 from langchain_core.runnables.base import Runnable
 from langchain_core.output_parsers import StrOutputParser
+from ollama import Client as OllamaClient
 
 
 @st.cache_resource
@@ -46,6 +47,19 @@ def get_model(model: str, temperature: int) -> ChatOllama:
     model = ChatOllama(model=model, temperature=temperature)
     return model
 
+@st.cache_data
+def get_available_models():
+    ollama_client = OllamaClient()
+    available_models = ollama_client.list()["models"]
+    available_models = [model["model"] for model in available_models]
+    if len(available_models) == 0:
+        raise ValueError("""No models available", please pull one from ollama using the cli. For example:
+                         ```bash
+                         ollama pull llama3.2:1b
+                         ```
+                         """)
+    return available_models
+
 whatsapp_client = load_client()
 
 
@@ -58,7 +72,7 @@ start = st.sidebar.date_input("Start date", datetime.now() - timedelta(days=365)
 end = st.sidebar.date_input("End date", datetime.now())
 
 st.sidebar.subheader("Model Options")
-model_name = st.sidebar.selectbox("Select a model", ["llama3.2:1b", "deepseek-r1:1.5b"])
+model_name = st.sidebar.selectbox("Select a model", get_available_models())
 temp = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.8)
 ollama_model = get_model(model_name, temp)
 
